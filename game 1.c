2613c5f517b0658e2856f4117832951837a8d29b
wink haha <yangzlin767@gmail.com>
2025-10-26 22:35:56 +0800

    ✨ feat:

--- game 1.c
+++ game 1.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
-menu()
+void menu()
 {
     printf("*****************************\n");
     printf("****** 1.play  0.exit  ******\n");
     printf("*****************************\n");
+}
+void game()
+{
+    
 }
 int main()
 {
@@ -13,6 +17,19 @@ int main()
         menu();//打印菜单
         printf("请选择>:");
         scanf("%d",&input);
-    } while ();
+        switch(input)
+        {
+            case 1:
+            game( );
+            break;
+            case 2:
+            printf("退出游戏\n");
+            break;
+            default:
+            printf("选择错误\n");
+            break;
+
+        }
+    } while (input);
     return 0;
 }
