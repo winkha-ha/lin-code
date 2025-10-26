1053881c51c02b91329c34079188b08c2f002153
wink haha <yangzlin767@gmail.com>
2025-10-26 21:01:08 +0800

    ✨ feat:

--- 1023.c
+++ 1023.c
@@ -71,3 +71,76 @@
 // 已知天干的顺序是：甲、乙、丙、丁、戊、己、庚、辛、壬、癸；地支的顺序是：子、丑、寅、卯、辰、巳、午、未、申、酉、戌、亥。将天干和地支连起来，就组成了一个天干地支的年份，
 // 例如2020年是庚子年，每过一年，天干和地支都会移动到下一个，例如2021年是辛丑年。输入一个年份，输出这一年的天干地支年份。（要求用switch语句完成）
 // 如输入：2023   输出：2023年是癸卯年
+// #include<stdio.h>
+// int main()
+// {
+//     int year = 0;
+//     scanf("%d", &year );
+
+//     int gan = (year - 4) % 10;
+//     int zhi = (year - 4) % 12;
+
+//     if( gan<0 )gan+=10;
+//     if( zhi<0 )zhi+=15;
+
+//     printf("%d年是", year);
+
+//     switch (gan)
+//     {
+//         case 0:printf("甲");break;
+//         case 1:printf("乙");break;
+//         case 2:printf("丙");break;
+//         case 3:printf("丁");break;
+//         case 4:printf("戊");break;
+//         case 5:printf("己");break;
+//         case 6:printf("庚");break;
+//         case 7:printf("辛");break;
+//         case 8:printf("壬");break;
+//         case 9:printf("癸");break;
+
+//     }
+
+//     switch (zhi)
+//     {
+//         case 0: printf("子");break;
+//         case 1: printf("丑");break;
+//         case 2: printf("寅");break;
+//         case 3: printf("卯");break;
+//         case 4: printf("辰");break;
+//         case 5: printf("巳");break;
+//         case 6: printf("午");break;
+//         case 7: printf("未");break;
+//         case 8: printf("申");break;
+//         case 9: printf("酉");break;
+//         case 10: printf("戌");break;
+//         case 11: printf("亥");break;
+//     }
+//     printf("年\n");
+//     return 0;
+// }
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+ int main()
+ {
+    int a,b;
+    srand(time(NULL));//取随机数
+    a = rand()%100+1;//范围是1~100
+    scanf("%d", &b);
+    if( a>b )
+    {
+        printf("很遗憾，你的答案小于此数\n");
+        printf("这个随机数为%d", a);
+    }
+    else if( a<b )
+    {
+        printf("很遗憾，你的答案大于此数\n");
+        printf("这个随机数是%d", a);
+    }
+    else if( a==b )
+    {
+        printf("恭喜你猜对了！");
+    }
+
+    return 0;
+ }
